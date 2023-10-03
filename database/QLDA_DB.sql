bb275ce47a8128ae9d9b81024d39f4509bb9527f
Dang Nguyen Quang Huy <hdang1696@gmail.com>
2023-10-03 13:32:07 +0700

    Create Table NhiemVu,Sprint,TAiNguyen,CAp

--- database/QLDA_DB.sql
+++ database/QLDA_DB.sql
@@ -3,3 +3,34 @@ GO
 
 USE QLDA;
 GO
+Create Table NHIEMVU
+( 
+  MaNhiemVu  varchar(10) primary key,
+  MaTienQuyet varchar(10) ,
+  TrangThai nvarchar(30) not null,
+  ThoiGianLamThucTe time not null,
+  TenNhiemVu nvarchar not null,
+  ThoiGianUocTinh time not null,
+  MaNV varchar(10),
+  MaCV varchar(10),
+)
+Create TABLE SPRINT 
+(
+	MaSprint varchar(10) primary key,
+	NoiDung nvarchar(30),
+	NgayBD date ,
+	NgayKT date,
+	MaDA varchar(10)
+)
+Create Table TAINGUYEN 
+(
+	MaTN varchar(10) primary key,
+	TenTN nvarchar(20) not null,
+	LoaiTaiNguyen nvarchar(20) not null,
+)
+CREATE TABLE CAP
+(
+	MaDA varchar(10),
+	MaTN varchar(10),
+	primary key(MaDA,MaTN)
+)
