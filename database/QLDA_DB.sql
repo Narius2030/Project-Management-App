fccc6d7b47f5745e0e74b6530cbec356c9a4967a
Narius2030 <buiducnhan15122003@gmail.com>
2023-10-03 13:29:58 +0700

    Creating tables NHANVIEN, TAIKHOAN, TEAM, TEAMLEADER

--- database/QLDA_DB.sql
+++ database/QLDA_DB.sql
@@ -3,3 +3,36 @@ GO
 
 USE QLDA;
 GO
+
+CREATE TABLE NHANVIEN (
+	MaNV varchar(10) PRIMARY KEY,
+	HovaTenDem nvarchar(25) NOT NULL,
+	Ten nvarchar(25) NOT NULL,
+	Email varchar(25),
+	Levels varchar(10),
+	DiaChi nvarchar(50),
+	SDT varchar(20) NOT NULL,
+);
+GO
+
+CREATE TABLE TAIKHOAN (
+	UserNames varchar(25) NOT NULL,
+	Passwords varchar(25) NOT NULL,
+	MaNV varchar(10)
+);
+GO
+
+CREATE TABLE TEAMLEADER (
+	TenNhom nvarchar(20) NOT NULL,
+	MaDA varchar(10),
+	MaNV varchar(10)
+);
+GO
+
+CREATE TABLE TEAM (
+	MaNV varchar(10),
+	TenNhom varchar(20),
+	MaDA varchar(10),
+	CapPerDay int NOT NULL
+);
+GO
