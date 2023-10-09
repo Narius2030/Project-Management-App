e9c8796dbed46a40737547dbd5c7b5861fd6944e
Dang Nguyen Quang Huy <hdang1696@gmail.com>
2023-10-09 20:59:10 +0700

    fixed

--- database/Constraints.sql
+++ database/Constraints.sql
@@ -28,7 +28,7 @@ SELECT *
 FROM NHANVIEN NV
 WHERE NOT EXISTS(
 	SELECT *
-	FROM DUAN AS pm, TEAM as t
+	FROM DUAN AS pm, TEAM AS t
 	WHERE pm.MaPM = NV.MaNV AND t.MaNV = NV.MaNV
 )
 go
@@ -100,7 +100,7 @@ AS
 SELECT *FROM TAINGUYEN,CAP,DUAN
 WHERE TAINGUYEN.MaTN=CAP.MaTN AND DUAN.MaDA=CAP.MaDA
 GO
---###Constraints
+--###Constraints CHECK
 -- câu 1: check tiến độ công việc và tiến độ dự án
 ALTER TABLE CONGVIEC ADD CONSTRAINT CHECK_TIENDOCV CHECK (TienDo<=100 and TienDo>=0)
 ALTER TABLE DUAN ADD CONSTRAINT CHECK_TIENDODA CHECK (TienDo <=100 and TienDo>=0)
@@ -121,7 +121,7 @@ go
 
 --###Triggers
 --1.Thêm mới thông tin trong bảng UOCLUONG (insert) khi thêm một nhân viên mới vào nhóm trong một dự án
-create trigger tr_addUocLuong on TEAMLEADER
+create trigger tr_addUocLuong on TEAM
 AFTER INSERT AS
 BEGIN
    insert into UOCLUONG
@@ -141,8 +141,8 @@ AS
 BEGIN
     IF EXISTS (SELECT * FROM deleted WHERE deleted.GiaiDoan NOT in ('Done', 'Delay'))
     BEGIN
-        print('Không thể xóa dự án');
-        ROLLBACK;
+        RAISERROR('Không thể xóa dự án',16,2);
+        ROLLBACK TRAN;
     END;
     
 END;
@@ -155,18 +155,20 @@ ON DUAN
 AFTER UPDATE
 AS
 BEGIN
+	DECLARE @MADA VARCHAR(10)
+	SELECT @MADA=inserted.MaDA FROM inserted
     IF EXISTS ( SELECT * FROM inserted WHERE TienDo = 100
     )
     BEGIN
         UPDATE DUAN
         SET GiaiDoan = 'Done'
         FROM DUAN
-        JOIN inserted ON DUAN.MaDA = inserted.MaDA;
+        WHERE DUAN.MaDA=@MADA
     END
 	ELSE
 	 BEGIN
-        print('Không thể cập nhật dự án');
-        ROLLBACK;
+        RAISERROR('Không thể cập nhật dự án',16,2);
+        ROLLBACK TRAN;
     END;
 END;
 go
@@ -178,12 +180,12 @@ AS
 BEGIN
     IF EXISTS (
         SELECT *
-        FROM inserted as i
+        FROM deleted as i
         WHERE i.GiaiDoan <> 'Done' and i.TienDo <> 100
     )
     BEGIN
-        PRINT('Không thể thiết lập giai đoạn mới có thể do nhiệm vụ vẫn chưa được hoàn thành')
-        ROLLBACK;
+        RAISERROR('Không thể thiết lập giai đoạn mới có thể do nhiệm vụ vẫn chưa được hoàn thành')
+        ROLLBACK TRAN;
     END
 END;
 GO
@@ -195,7 +197,7 @@ BEGIN
 	                   WHERE nv.TrangThai not in ('Done'))
 	BEGIN
 	      PRINT('Không thể xóa công việc vì nhiệm vụ chưa được hoàn thành!')
-          ROLLBACK
+          ROLLBACK TRAN
     END
 END
 go
@@ -296,20 +298,20 @@ BEGIN
 
     -- Lấy MaDA từ bảng inserted
     DECLARE @madamoithem INT
-	DECLARE @trangthaicv varchar(30)
-    SELECT @madamoithem = MaDA FROM inserted;
+	DECLARE @NGAYKETHUC DATE
+    SELECT @madamoithem = inserted.MaDA FROM inserted;
 
     -- Tạo con trỏ trên  danh sách ngày kết thúc từ bảng SPRINT với điều kiện cùng 1 mã dự án
     --DECLARE cur CURSOR FOR
-    SELECT @trangthaicv=cv.TrangThai FROM CONGVIEC AS CV,inserted AS S
-	WHERE CV.MaSprint=S.MaSprint  AND S.MaDA=@madamoithem and CV.TrangThai!='Done'
+    SELECT @NGAYKETHUC=S.NgayKT FROM DUAN ,inserted AS S
+	WHERE  S.MaDA=@madamoithem
     OPEN cur
 	--đặt con trỏ vào hàng đầu tiên 
-    FETCH NEXT FROM cur INTO @trangthaicv
+    FETCH NEXT FROM cur INTO @NGAYKETHUC
     WHILE @@FETCH_STATUS = 0
     BEGIN
         -- So sánh ngày kết thúc với ngày hiện tại
-        IF @trangthaicv !='Done'
+        IF @NGAYKETHUC >= GETDATE()
 
         BEGIN
 			 RAISERROR('Lỗi Sprint của giai đoạn trước thuộc dự án này chưa kết thúc.', 16, 1)
