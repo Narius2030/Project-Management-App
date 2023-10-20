cb4c2329e1ee8c73cce666aa2ee423e42bf1fc0c
Narius2030 <buiducnhan15122003@gmail.com>
2023-10-20 10:24:43 +0700

    Updating project

---
+++ QuanLyCT/Views/Custome_control/CustomTextBox.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLCongTy
+{
+    public partial class CTTextBox : UserControl
+    {
+        //Constructor
+        public CTTextBox()
+        {
+            InitializeComponent();
+        }
+
+        //Fields
+        private Color borderColor = Color.MediumSlateBlue;
+        private int borderSize = 2;
+        private bool underlinedStyle = false;
+        //private bool password = false;
+        //private bool multiline = false;
+
+        //Properties
+        public Color BorderColor 
+        {
+            get
+            {
+                return borderColor;
+            }
+            set
+            {
+                borderColor = value;
+                this.Invalidate();
+            }
+        }
+        public int BorderSize
+        {
+            get
+            {
+                return borderSize;
+            }
+            set
+            {
+                borderSize = value;
+                this.Invalidate();
+            }
+        }
+        public bool UnderlinedStyle
+        {
+            get
+            {
+                return underlinedStyle;
+            }
+            set
+            {
+                underlinedStyle = value;
+                this.Invalidate();
+            }
+        }
+
+        public bool Password
+        {
+            get
+            {
+                return textbox.UseSystemPasswordChar;
+            }
+            set
+            {
+                textbox.UseSystemPasswordChar = value; 
+                this.Invalidate();
+            }
+        }
+
+        public bool Multiline
+        {
+            get
+            {
+                return textbox.Multiline;
+            }
+            set
+            {
+                textbox.Multiline = value;
+            }
+        }
+
+        public override Color BackColor
+        {
+            get
+            {
+                return base.BackColor;
+            }
+            set
+            {
+                base.BackColor = value;
+                textbox.BackColor = value;
+            }
+        }
+
+        public override Color ForeColor
+        {
+            get
+            {
+                return base.ForeColor;
+            }
+            set
+            {
+                base.ForeColor = value;
+                textbox.ForeColor = value;
+            }
+        }
+
+        public override Font Font
+        {
+            get
+            {
+                return base.Font;
+            }
+            set
+            {
+                base.Font = value;
+                textbox.Font = value;
+                if (this.DesignMode)
+                {
+                    UpdateControlHeight();
+                }
+            }
+        }
+
+        public string Texts
+        {
+            get
+            {
+                return textbox.Text;
+            }
+            set
+            {
+                textbox.Text = value;
+            }
+        }
+
+        //Overide methods
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics graph = e.Graphics;
+
+            //Draw border
+            using (Pen penBorder = new Pen(borderColor, borderSize))
+            {
+                penBorder.Alignment = System.Drawing.Drawing2D.PenAlignment.Inset;
+
+                if (underlinedStyle)
+                {
+                    graph.DrawLine(penBorder, 0, this.Height - 1, this.Width, this.Height - 1);
+                }
+                else
+                {
+                    graph.DrawRectangle(penBorder, 0, 0, this.Width - 0.5F, this.Height - 0.5F);
+                }
+            }
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            if (this.DesignMode)
+            {
+                UpdateControlHeight();
+            }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            UpdateControlHeight();
+        }
+
+        //private mothods
+        private void UpdateControlHeight()
+        {
+            if (textbox.Multiline == false)
+            {
+                int txtHeight = TextRenderer.MeasureText("Text", this.Font).Height + 1;
+                textbox.Multiline = true;
+                textbox.MinimumSize = new Size(0, txtHeight);
+                textbox.Multiline = false;
+
+                this.Height = textbox.Height + this.Padding.Top + this.Padding.Bottom;
+            }
+        }
+
+        private void CTTextBox_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
