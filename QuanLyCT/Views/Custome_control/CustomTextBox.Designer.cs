a1a340cf6be253f94fcb473837383539f32ae0e7
Narius2030 <buiducnhan15122003@gmail.com>
2023-10-20 10:20:18 +0700

    Adding project

---
+++ QuanLyCT/Views/Custome_control/CustomTextBox.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace QLCongTy
+{
+    partial class CTTextBox
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textbox
+            // 
+            this.textbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textbox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textbox.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textbox.Location = new System.Drawing.Point(7, 7);
+            this.textbox.Name = "textbox";
+            this.textbox.Size = new System.Drawing.Size(227, 27);
+            this.textbox.TabIndex = 0;
+            // 
+            // CTTextBox
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.textbox);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "CTTextBox";
+            this.Padding = new System.Windows.Forms.Padding(7);
+            this.Size = new System.Drawing.Size(241, 35);
+            this.Load += new System.EventHandler(this.CTTextBox_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textbox;
+    }
+}
---
+++ QuanLyCT/Views/DiemDanh/fCheckin-Checkout.cs
@@ -0,0 +1,47 @@
+﻿using QLCongTy.QLDuAn;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLCongTy
+{
+    public partial class fCheckin_Checkout : Form
+    {
+      
+        public fCheckin_Checkout()
+        {
+            InitializeComponent();
+        }
+
+        private void fCheckin_Checkout_Load(object sender, EventArgs e)
+        {
+            ReLoad();
+            txtManvsang.Texts = fMainMenu.MaNV;
+            txtMacvsang.Texts = fMainMenu.MaCV;
+        }
+
+        public void ReLoad()
+        {
+
+        }
+
+        private void btnSubmitSang_Click(object sender, EventArgs e)
+        {
+           
+            //Thông báo
+            MessageBox.Show("Đã check in thành công");
+            ReLoad();
+        }
+
+        private void tpCheckSang_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
