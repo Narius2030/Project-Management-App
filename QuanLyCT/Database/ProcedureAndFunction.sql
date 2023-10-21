d4e15ae787e1b60778431fe9f293dabdfa1680d8
Dang Nguyen Quang Huy <hdang1696@gmail.com>
2023-10-21 21:21:20 +0700

    Update DB

---
+++ QuanLyCT/Database/ProcedureAndFunction.sql
@@ -0,0 +1,9 @@
+﻿--Procedure Dang Nhap co tham số đầu vào trả ra 1 giá trị cụ thể
+CREATE OR ALTER PROCEDURE sp_ktrDangNhap
+@matk VARCHAR(20), @matkhau VARCHAR(20), @check INT OUTPUT
+AS
+BEGIN
+	SELECT @check=COUNT(*) FROM NHANVIEN
+	WHERE TaiKhoan = @matk AND MatKhau = @matkhau
+END
+GO
